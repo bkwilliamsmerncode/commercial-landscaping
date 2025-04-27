787f258c140374561707a4de9274ca4068ab937d
Brian Williams <152632135+bkwilliamsmerncode@users.noreply.github.com>
2025-04-26 21:01:00 -0700

    got rid of scrollbars, changed the logo, made some minor tweeks

--- src/Components/Home/HomeContent.jsx
+++ src/Components/Home/HomeContent.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react';
 import "./HomeContent.css"
+import Modal from '../Modal/Modal';
 
 const HomeContent = () => {
+
+const [showModal, setShowModal] = useState(false);
+
+const handleModal = () => {
+  setShowModal(!showModal);
+}
+
   return (
     <>
+    {showModal && <Modal onClose={handleModal} />}
     <div className='xlscreens'>
       <div className='homeMain'>
         <div className='hero'>
 <div className="heroText">
   <div className="companyName">FourFront Landscaping</div>
   <div className="companySlogan">Landscaping Done Right!</div>
-  <button className='formBtn'>Get A Free Estimate</button>
+  <button className='formBtn'  onClick={handleModal}>Get A Free Estimate</button>
 </div>
         </div>
         </div>
