4e3485ec345ff741dc42e1d586c2c3cfb075e412
Brian Williams <152632135+bkwilliamsmerncode@users.noreply.github.com>
2025-05-21 19:05:26 -0700

    changed out and added some pictures

--- src/Components/ImageCarousel/ImageCarousel.jsx
+++ src/Components/ImageCarousel/ImageCarousel.jsx
@@ -6,6 +6,20 @@ import img3 from '../../Assets/img3.JPG';
 import img4 from '../../Assets/img4.JPG';
 import img5 from '../../Assets/img5.JPG';
 import img6 from '../../Assets/img6.JPG';
+import img7 from '../../Assets/img7.JPG';
+import img8 from '../../Assets/img8.JPG';
+import img9 from '../../Assets/img9.JPG';
+import img10 from '../../Assets/img10.JPG';
+import img11 from '../../Assets/img11.JPG';
+import img12 from '../../Assets/img12.JPG';
+import img13 from '../../Assets/img13.JPG';
+import img14 from '../../Assets/img14.JPG';
+import img15 from '../../Assets/img15.JPG';
+import img16 from '../../Assets/img16.JPG';
+import img17 from '../../Assets/img17.JPG';
+import img18 from '../../Assets/img18.JPG';
+import img19 from '../../Assets/img19.JPG';
+import img20 from '../../Assets/img20.JPG';
 import './ImageCarousel.css';
 
 const images = [
@@ -15,6 +29,20 @@ const images = [
     img1,
     img2,
     img3,
+    img7,
+    img8,
+    img9,
+    img10,
+    img11,
+    img12,
+    img13,
+    img14,
+    img15,
+    img16,
+    img17,
+    img18,
+    img19,
+    img20
     // Add more images as needed
 ];
 
